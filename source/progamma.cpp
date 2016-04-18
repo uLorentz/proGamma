ed1c51466b492b851b4d6a11f6a890e1646eff72
cubanpit <cubanspit@gmail.com>
2016-04-18 23:37:03 +0200

    Removed traling spaces, partial correction of mixed indentetion.

--- source/progamma.cpp
+++ source/progamma.cpp
@@ -1,3 +1,15 @@
+/******************************************************************************/
+/* Program for data analysis from '.Spe' file from Maestro ORTEC software.    */
+/* application: execute a fit and print some info                             */
+/* manage_flags: handle config flags                                          */
+/*                                                                            */
+/* Written by :                                                               */
+/*      Lorenzo Uboldi <lorenzo.uboldi@studenti.unimi.it>                     */
+/* Contribution by :                                                          */
+/*     Pietro F. Fontana <pietrofrancesco.fontana@studenti.unimi.it>          */
+/*                                                                            */
+/******************************************************************************/
+
 #include "functions.h"
 
 
